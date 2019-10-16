a00d477455b8c18ba7c8ebc216722ef55bf680e3
Ethan Zoller <ezoller11@gmail.com>
2019-10-16 09:43:27 -0700

    Changed Author and Browser Icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Otter Chaos - FRC 4512`,
     description: `Learning and striving for better steam through high school robotics.`,
-    author: `@gatsbyjs`,
+    author: `@otterchaos4512`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,11 +24,8 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.svg`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
