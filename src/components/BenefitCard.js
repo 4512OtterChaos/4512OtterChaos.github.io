270c41befdf7724361b5ab3cbc4f52c4cc7cc1d8
Ethan Zoller <ezoller11@gmail.com>
2019-10-16 21:23:16 -0700

    Added Benefit Card Component

---
+++ src/components/BenefitCard.js
@@ -0,0 +1,42 @@
+import React from "react"
+import styled from "styled-components"
+import Layout from "./layout"
+
+const Header = ({ title, text, img, alt }) => {
+  return (
+    <Layout>
+      <Card>
+        <Content>
+          <img src={img} alt={alt} />
+          <h3>{title}</h3>
+          <p>{text}</p>
+        </Content>
+      </Card>
+    </Layout>
+  )
+}
+
+export default Header
+
+const Card = styled.div`
+  background: var(--main-grey-light);
+  height: 180px;
+  width: 330px;
+  border-radius: 8px;
+  margin: 8px 8px 0 0;
+`
+
+const Content = styled.div`
+  padding: 30px 20px;
+
+  h3 {
+    font-size: 16px;
+  }
+
+  p {
+    font-size: 16px;
+    padding: 0;
+    margin: -15px 0;
+    line-height: 22px;
+  }
+`
