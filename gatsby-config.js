462efc208b8c88837f7e058471c2639d1c9df383
Ethan Zoller <ezoller11@gmail.com>
2019-10-24 20:32:48 -0700

    Changed Color

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#0066ff`,
+        theme_color: `#0066ff`,
         display: `minimal-ui`,
         icon: `src/images/logos/logo.svg`, // This path is relative to the root of the site.
       },
