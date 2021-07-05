dd0fd44b7ce62e4ad243a32eaa177172aa1550a9
amberquake <noleetarrr@gmail.com>
2021-07-05 04:06:11 -0700

    added riptide to robots, some refitting

--- src/pages/robots.js
+++ src/pages/robots.js
@@ -1,5 +1,6 @@
 import React from "react"
 import styled, { keyframes } from "styled-components"
+import ReactPlayer from "react-player"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -12,92 +13,143 @@ import Pneutron from "../images/graphics/pneutron.png"
 import Pneutron2 from "../images/graphics/pneutron2.png"
 import Dugg from "../images/graphics/dugg.png"
 import Dugg2 from "../images/graphics/dugg2.png"
+import Riptide from "../images/graphics/riptide.png"
+import Riptide2 from "../images/graphics/riptide2.png"
 
 const RobotsPage = () => (
-  <Layout>
-    {/* SEO */}
-    <SEO title="Robots" />
+    <Layout>
+        {/* SEO */}
+        <SEO title="Robots" />
 
-    {/* Header */}
-    <Header />
+        {/* Header */}
+        <Header />
 
-    {/* Hero */}
-    <Hero>
-      <h1>Robots</h1>
-      <p>
-        If you want to learn more about the specifics,{" "}
-        <a href="/contact">contact us!</a>
-      </p>
-    </Hero>
+        {/* Title */}
+        <Hero>
+            <h1>Robots</h1>
+            <p>
+                Check out what we're up to on 
+                <a href="https://www.youtube.com/user/Bearbots"> youtube</a>
+                , and see our robot code on
+                <a href="https://github.com/4512OtterChaos"> github</a>
+                .
+            </p>
+        </Hero>
 
-    <TextContainer
-      caption="Destination Deep Space - 2019"
-      title="Pneutron"
-      paragraph="This robot was for the 2019 FIRST Robotics Competition. The game involves two alliances of three teams each competing to place hatch covers and rubber 
-balls or “cargo” on rockets and cargo ships before returning to their hab platform to climb at the end of the match."
-    />
+        {/* Robot images with description */}
+        <TitledRobot>
+            <TextContainer
+                caption="Infinite Recharge - 2020/2021"
+                title="Riptide"
+                paragraph="This robot was for the 2020/2021 FIRST Robotics Competition. The game involves two alliances of three
+                teams shooting foam dodgeballs into high/low goals for points before attempting to climb and balance a swinging bar."
+            />
 
-    <RobotImages>
-      <img src={Pneutron} alt="Robot Front View" />
-      <img src={Pneutron2} alt="Robot Back View" />
-    </RobotImages>
+            <RobotImages>
+                <img src={Riptide} alt="Robot Front View" />
+                <VideoContainer>
+                    <ReactPlayer
+                        url="https://youtu.be/mPWVxLnCw0A"
+                        width="100%"
+                        height="100%"
+                    />
+                </VideoContainer>
+                <img src={Riptide2} alt="Robot Back View" />
+            </RobotImages>
+        </TitledRobot>
 
-    <TextContainer
-      caption="Power Up - 2018"
-      title="DUGG"
-      paragraph="This robot was for the 2018 FIRST Robotics Competition. The game involves two alliances of three teams each competing to place power cubes on large balancing scales to tip the scale to gain ownership or trade them in for powerups in order to win."
-    />
+        <TitledRobot>
+            <TextContainer
+                caption="Destination Deep Space - 2019"
+                title="Pneutron"
+                paragraph="This robot was for the 2019 FIRST Robotics Competition. The game involves two alliances of three teams each competing to place hatch covers and rubber 
+                balls or “cargo” on rockets and cargo ships before returning to their hab platform to climb at the end of the match."
+            />
 
-    <RobotImages>
-      <img src={Dugg} alt="Robot Front View" />
-      <img src={Dugg2} alt="Robot Back View" />
-    </RobotImages>
+            <RobotImages>
+                <img src={Pneutron} alt="Robot Front View" />
+                <img src={Pneutron2} alt="Robot Back View" />
+            </RobotImages>
+        </TitledRobot>
 
-    {/* Footer */}
-    <Footer />
-  </Layout>
+        <TitledRobot>
+            <TextContainer
+                caption="Power Up - 2018"
+                title="DUGG"
+                paragraph="This robot was for the 2018 FIRST Robotics Competition. The game involves two alliances of three
+                teams each competing to place power cubes on large balancing scales to tip the scale to gain ownership or trade them in for powerups in order to win."
+            />
+
+            <RobotImages>
+                <img src={Dugg} alt="Robot Front View" />
+                <img src={Dugg2} alt="Robot Back View" />
+            </RobotImages>
+        </TitledRobot>
+
+        {/* Footer */}
+        <Footer />
+    </Layout>
 )
 
 export default RobotsPage
 
-const HeroAnimation = keyframes`
-  from {
-    opacity: 0;
-    transform: translateY(20px);
-  }
+const EnterAnimation = keyframes`
+    from {
+        opacity: 0;
+        transform: translateY(3vh);
+    }
 
-  to {
-    opacity: 1;
-    transform: translateY(0px);
-  }
+    to {
+        opacity: 1;
+        transform: translateY(0px);
+    }
 `
 
 const Hero = styled.div`
-  margin: 10vmin auto;
-  max-width: 768px;
-  text-align: center;
-  animation: ${HeroAnimation} 3s forwards cubic-bezier(0.2, 0.8, 0.2, 1);
+    margin: 6vh auto;
+    max-width: 50%;
+    text-align: center;
+    opacity: 0;
+    animation: ${EnterAnimation} 3s forwards cubic-bezier(0.2, 0.8, 0.2, 1);
 
-  h1 {
-    margin-top: 10px;
-    margin-bottom: 10px;
-    animation: ${HeroAnimation} 1s forwards cubic-bezier(0.2, 0.8, 0.2, 1);
-  }
+    h1 {
+        margin: 1vh auto;
+        animation: ${EnterAnimation} 1s forwards cubic-bezier(0.2, 0.8, 0.2, 1);
+    }
 
-  p {
-    max-width: 700px;
-    margin: 0 auto;
-    animation: ${HeroAnimation} 3s forwards cubic-bezier(0.2, 0.8, 0.2, 1);
-  }
+    p {
+        max-width: 80%;
+        margin: 0 auto;
+        animation: ${EnterAnimation} 3s forwards cubic-bezier(0.2, 0.8, 0.2, 1);
+    }
+`
+
+const TitledRobot = styled.div`
+    opacity: 0;
+    animation: ${EnterAnimation} 3s forwards cubic-bezier(0.2, 0.8, 0.2, 1);
+    animation-delay: 0.75s;
 `
 
 const RobotImages = styled.div`
-  margin: 0 auto 24px;
-  max-width: 1024px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
+    margin: 0 auto 2vh;
+    max-width: 1200px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+	align-items: center;
 
-  img {
-    width: 100%;
-  }
+    img {
+        width: calc(20% + 10vmin);
+    }
+
+    @media (max-width: 1320px){
+        max-width: 85%;
+    }
 `
+
+const VideoContainer = styled.div`
+    margin: auto 2%;
+    width: calc(20% + 10vmin);
+    height: calc(20vmin);
+`
+    
