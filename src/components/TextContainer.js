426571a5c8b8da43c3a767d901b1d9020f23ca38
amberquake <noleetarrr@gmail.com>
2021-07-05 04:05:17 -0700

    change width queries

--- src/components/TextContainer.js
+++ src/components/TextContainer.js
@@ -3,38 +3,39 @@ import styled from "styled-components"
 import Layout from "./layout"
 
 const TextContainer = ({ caption, title, paragraph }) => {
-  return (
-    <Layout>
-      <Container>
-        <span>{caption}</span>
-        <h2>{title}</h2>
-        <p>{paragraph}</p>
-      </Container>
-    </Layout>
-  )
+    return (
+        <Layout>
+            <Container>
+                <span>{caption}</span>
+                <h2>{title}</h2>
+                <p>{paragraph}</p>
+            </Container>
+        </Layout>
+    )
 }
-
+    
 export default TextContainer
 
 const Container = styled.div`
-  margin: 10vmin auto 24px;
-  max-width: 1024px;
-
-  span {
-    font-family: "San Francisco", sans-serif;
-    color: var(--main-blue-dark);
-    font-weight: 700;
-  }
-
-  h2 {
-    margin: 0.6vh 0 2vh 0;
-  }
-
-  p {
-    margin-top: -1.4vh;
-  }
-
-  @media (max-width: 1124px) {
-    width: 90%;
-  }
+    margin: calc(5vmin + 5vh) auto calc(1vh + 2vmin);
+    max-width: 50%;
+    
+    span {
+        font-family: "San Francisco", sans-serif;
+        color: var(--main-blue-dark);
+        font-weight: 700;
+    }
+    
+    h2 {
+        margin: 0.6vh 0 2vh 0;
+    }
+    
+    p {
+        margin-top: -1.4vh;
+    }
+    
+    @media (max-width: 1400px) {
+        max-width: 70%;
+    }
 `
+    
