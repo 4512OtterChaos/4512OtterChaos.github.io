01cea44ed5e85af7d6ab6a33dc1684e7eecddddf
ProgrammingProjects0 <98847024+ProgrammingProjects0@users.noreply.github.com>
2022-02-14 16:53:26 -0800

    add mentor

--- src/pages/team.js
+++ src/pages/team.js
@@ -72,6 +72,7 @@ const TeamPage = () => (
 			<Card name="Patrick Naughton" title="Design" />
 			<Card name="Nolan Brown" title="Programming" />
 			<Card name="Henry Beason" title="Design" />
+			<Card name="Romy Beigel"/>
 		</CardContainer>
 	</TitledCard>
 	
